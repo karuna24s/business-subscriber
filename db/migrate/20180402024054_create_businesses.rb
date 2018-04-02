6785f8e92c9b5222c401562178fa1a0766ec14f3
karuna24s <karuna24s@gmail.com>
2018-04-01 23:09:13 -0400

    add migrations for business and subscribers

--- db/migrate/20180402024054_create_businesses.rb
+++ db/migrate/20180402024054_create_businesses.rb
@@ -1,7 +1,8 @@
 class CreateBusinesses < ActiveRecord::Migration[5.1]
   def change
     create_table :businesses do |t|
-
+      t.string :business_name
+      t.integer :status
       t.timestamps
     end
   end
--- db/migrate/20180402024106_create_subscribers.rb
+++ db/migrate/20180402024106_create_subscribers.rb
@@ -1,7 +1,9 @@
 class CreateSubscribers < ActiveRecord::Migration[5.1]
   def change
     create_table :subscribers do |t|
-
+      t.string :name
+      t.string :email_address
+      t.integer :business_id
       t.timestamps
     end
   end
